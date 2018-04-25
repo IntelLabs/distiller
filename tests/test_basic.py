8d831426818e45410cb076d38298921ee5b12007
Neta Zmora <neta.zmora@intel.com>
2018-04-25 12:00:19 +0300

    Fixed test execution in new environment

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -15,6 +15,11 @@
 #
 
 import torch
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
 import distiller
 
 def test_sparsity():
