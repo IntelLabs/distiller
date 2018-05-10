195ed767f1dd84c1bc151ec8fbdbced42f3d1e7c
Neta Zmora <neta.zmora@intel.com>
2018-05-10 18:25:03 +0300

    pytorch 0.4: adjustments to API changes
    Various small changes due to the chamnges in the semantics and syntax of the
    PyTorch 0.4 API.
    
    Note that currently distiller.model_performance_summary() returns wrong results
    on graphs containing torch.nn.DataParallel layers.

--- distiller/model_summaries.py
+++ distiller/model_summaries.py
@@ -43,7 +43,7 @@ def model_summary(model, optimizer, what, dataset=None):
         distiller.log_weights_sparsity(model, -1, loggers=[pylogger, csvlogger])
     elif what == 'compute':
         if dataset == 'imagenet':
-            dummy_input = Variable(torch.randn(1, 3, 224, 224), requires_grad=False)
+            dummy_input = Variable(torch.randn(1, 3, 224, 224))
         elif dataset == 'cifar10':
             dummy_input = Variable(torch.randn(1, 3, 32, 32))
         else:
@@ -101,9 +101,9 @@ def weights_sparsity_summary(model, return_total_sparsity=False, param_dims=[2,4
                 distiller.sparsity_2D(param)*100,
                 distiller.sparsity_3D(param)*100,
                 (1-_density)*100,
-                param.std(),
-                param.mean(),
-                param.abs().mean()
+                param.std().item(),
+                param.mean().item(),
+                param.abs().mean().item()
             ])
 
     total_sparsity = (1 - sparse_params_size/params_size)*100
@@ -158,7 +158,7 @@ def module_visitor(self, input, output, df, model, weights_vol, macs, attrs=None
     in_features_shape = input[0].size()
     out_features_shape = output.size()
 
-    param_name = distiller.model_find_param_name(model, self.weight.data)
+    param_name = distiller.model_find_param_name(model, self.weight)
     if param_name is None:
         return
     mod_name = param_name[:param_name.find(".weight")]
@@ -168,8 +168,13 @@ def module_visitor(self, input, output, df, model, weights_vol, macs, attrs=None
                               distiller.size_to_str(out_features_shape), distiller.volume(output),
                               weights_vol, int(macs)])
 
+
 def model_performance_summary(model, dummy_input, batch_size=1):
-    """Collect performance data"""
+    """Collect performance data
+
+    warning: in PyTorch 0.4 this function does not return correct values when
+    the graph contains torch.nn.DataParallel layers.
+    """
     def install_perf_collector(m):
         if isinstance(m, torch.nn.Conv2d):
             hook_handles.append(m.register_forward_hook(
