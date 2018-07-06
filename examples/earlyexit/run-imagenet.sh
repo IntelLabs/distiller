2c3cafa16a6347a3d2f7fd26d159357191e8e783
haim.barad <haim.barad@intel.com>
2018-07-06 00:26:18 -0700

    Fixed path to imagenet

--- examples/earlyexit/run-imagenet.sh
+++ examples/earlyexit/run-imagenet.sh
@@ -11,4 +11,4 @@ fullstring2="${threshold0}_${threshold1}"
 python -u earlyexit.py --pretrained --batch-size=64 --earlyexit $threshold0 $threshold1 \
     --arch=resnet50 --lossweights $lossweight0 $lossweight1 \
     --checkpointdir=/public/barad/$fullstring --lr=0.01 \
-    --name=/public/barad/$fullstring /public/barad/i1k-extracted/ |& tee -a /public/barad/$fullstring/log_$fullstring2
+    --name=/public/barad/$fullstring /public/aeon_I1K/i1k-extracted/ |& tee -a /public/barad/$fullstring/log_$fullstring2
