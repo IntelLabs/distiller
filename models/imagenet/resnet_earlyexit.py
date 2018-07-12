88f57d959a90085ebd71c07fde7c711f25915a98
Haim Barad <haim.barad@intel.com>
2018-07-12 13:24:20 +0300

    Make resnet50 name unique with exits

--- models/imagenet/resnet_earlyexit.py
+++ models/imagenet/resnet_earlyexit.py
@@ -3,7 +3,7 @@ import math
 import torch.utils.model_zoo as model_zoo
 
 
-__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
+__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50_earlyexit', 'resnet101', 'resnet152']
 
 # The following URLs represent pretrained models without exits and can't be used
 # directly for preloading as the topology is slightly altered to account
@@ -12,7 +12,7 @@ __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152
 model_urls = {
     'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
     'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet50_earlyexit': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
     'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
     'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
 }
@@ -199,7 +199,7 @@ def resnet34(pretrained=False, **kwargs):
     return model
 
 
-def resnet50(pretrained=False, **kwargs):
+def resnet50_earlyexit(pretrained=False, **kwargs):
     """Constructs a ResNet-50 model.
 
     Args:
