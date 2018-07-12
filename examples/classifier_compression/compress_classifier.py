40d33aefc04b94d3e8305a7e350f1e61c553b31a
Haim Barad <haim.barad@intel.com>
2018-07-12 13:41:20 +0300

    Include accuracy calculation

--- examples/classifier_compression/compress_classifier.py
+++ examples/classifier_compression/compress_classifier.py
@@ -641,6 +641,20 @@ def get_inference_var(tensor):
         return torch.autograd.Variable(tensor)
     return torch.autograd.Variable(tensor, volatile=True)
 
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
 
 if __name__ == '__main__':
     try:
