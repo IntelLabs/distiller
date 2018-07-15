fce7b614f53476ac3a61cff7e5bb21f42b9f7f2a
Haim Barad <haim.barad@intel.com>
2018-07-15 12:25:13 +0300

    clean up example code - all integrated into compression_classify

--- examples/earlyexit/cifar10/infer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for model in resnet20 resnet32 resnet44 resnet56 resnet110 resnet1202
-do
-  for threshold in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2
-  do
-    fullstring=$threshold.$model
-    python -u trainer.py --workers=1 --evaluate --earlyexitmodel=save_1.5.$model/model.th --earlyexit=$threshold --arch=$model |& tee results_$fullstring
-  done
-done
--- examples/earlyexit/imagenet/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# We currently focus on Resnet, but Early Exit
-# is applicable to other topologies as well.
-#from .alexnet import *
-from .resnet import *
-#from .vgg import *
-#from .squeezenet import *
-#from .inception import *
-#from .densenet import *
