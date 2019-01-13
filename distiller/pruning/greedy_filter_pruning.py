d0f383413f7e97dcff5d7ef195944141970171dc
Neta Zmora <neta.zmora@intel.com>
2019-01-13 15:57:13 +0200

    Prepare Greedy Pruning for parallelization

--- distiller/pruning/greedy_filter_pruning.py
+++ distiller/pruning/greedy_filter_pruning.py
@@ -16,10 +16,14 @@ from copy import deepcopy
 import numpy as np
 import logging
 import torch
+import torch.multiprocessing as mp
+from multiprocessing import Process, Queue
+import multiprocessing
 import csv
 import os
 import distiller
 from apputils import SummaryGraph, save_checkpoint
+from distiller.data_loggers import PythonLogger
 from distiller import normalize_module_name
 
 __all__ = ['add_greedy_pruner_args', 'greedy_pruner']
@@ -54,43 +58,86 @@ def prune_tensor(param, param_name, fraction_to_prune, zeros_mask_dict):
                                                                 desired_sparsity=fraction_to_prune,
                                                                 weights=param_name)
     pruner.set_param_mask(param, param_name, zeros_mask_dict, meta=None)
-
     # Use the mask to prune
     zeros_mask_dict[param_name].apply_mask(param)
     return distiller.sparsity_3D(param)
 
 
-def find_most_robust_layer(model, pruning_step, test_func, train_fn, app_args, tensors_to_prune=None):
+def prune_finetune_test(iteration, model_cpy, pruning_step, test_fn, train_fn,
+                        app_args, param_name, training_epoch_duration):
+    pylogger = PythonLogger(msglogger)
+    zeros_mask_dict = distiller.create_model_masks_dict(model_cpy)
+    param = model_cpy.state_dict()[param_name]
+    if 0 == prune_tensor(param, param_name, pruning_step, zeros_mask_dict):
+        return (-1, -1, -1)  # Did not prune anything
+
+    if train_fn is not None:
+        # Fine-tune
+        optimizer = torch.optim.SGD(model_cpy.parameters(), lr=app_args.lr,
+                                    momentum=app_args.momentum, weight_decay=app_args.weight_decay)
+        app_args.training_epoch_duration = training_epoch_duration
+        train_fn(model=model_cpy, compression_scheduler=create_scheduler(model_cpy, zeros_mask_dict),
+                 optimizer=optimizer, epoch=iteration, loggers=[pylogger])
+
+    # Test and record the performance of the pruned model
+    prec1, prec5, loss = test_fn(model=model_cpy, loggers=None)
+    return (prec1, prec5, loss, zeros_mask_dict)
+
+
+def find_most_robust_layer(iteration, model, pruning_step, test_fn, train_fn,
+                           app_args, tensors_to_prune=None, training_epoch_duration=1):
     """Find the layer that is most robust to pruning 'pruning_step' filters.
 
     For each layer: prune 'step' percent of the filters, fine-tune, and measure top1 accuracy
     """
     if tensors_to_prune is None:
         tensors_to_prune = [param_name for param_name, _ in model.named_parameters()]
-    best_layer = (-np.inf, -np.inf, None, None, None)  # format: (prec1, prec5, param_name, pruned_model, zeros_mask_dict)
+    # best_layer format: (prec1, prec5, param_name, pruned_model, zeros_mask_dict)
+    best_layer = (-np.inf, -np.inf, None, None, None, np.inf)
     for param_name in tensors_to_prune:
         if model.state_dict()[param_name].dim() != 4:
             continue
-
         # Make a copy of the model, because when we prune the parameter tensor the model's weights are altered
         model_cpy = deepcopy(model)
+        (prec1, prec5, loss, zeros_mask_dict) = prune_finetune_test(iteration, model_cpy, pruning_step, test_fn,
+                                                                    train_fn, app_args, param_name,
+                                                                    training_epoch_duration)
+        if (prec1, prec5, -loss) > (best_layer[0], best_layer[1], -best_layer[-1]):
+            best_layer = (prec1, prec5, param_name, model_cpy, zeros_mask_dict, loss)
+    return best_layer[:-1]
+
+
+def find_most_robust_layer_mp(iteration, model, pruning_step, test_fn, train_fn,
+                              app_args, tensors_to_prune=None, training_epoch_duration=1):
+    """Multiprocessing version of find_most_robust_layer
+    """
+    if tensors_to_prune is None:
+        tensors_to_prune = [param_name for param_name, _ in model.named_parameters()]
+    # best_layer format: (prec1, prec5, param_name, pruned_model, zeros_mask_dict)
+    best_layer = (-np.inf, -np.inf, None, None, None, np.inf)
+    pqueue = Queue()
+    processes = []
+    for param_name in tensors_to_prune:
+        if model.state_dict()[param_name].dim() != 4:
+            continue
+        # Make a copy of the model, because when we prune the parameter tensor the model's weights are altered
+        model_cpy = deepcopy(model)
+        model_cpy.share_memory()
+        (prec1, prec5, loss, zeros_mask_dict) = prune_finetune_test(iteration, model_cpy, pruning_step,
+                                                                    test_fn, train_fn, app_args,
+                                                                    param_name, training_epoch_duration)
         param = model_cpy.state_dict()[param_name]
-        zeros_mask_dict = distiller.create_model_masks_dict(model_cpy)
-        if 0 == prune_tensor(param, param_name, pruning_step, zeros_mask_dict):
-            continue  # Did not prune anything
+        p = mp.Process(target=prune_finetune_test, args=(pqueue, model_cpy, pruning_step, test_fn,
+                                                         train_fn, app_args, param_name, param))
+        processes.append(p)
+        p.start()
 
-        if train_fn is not None:
-            # Fine-tune
-            optimizer = torch.optim.SGD(model_cpy.parameters(), lr=app_args.lr,
-                                        momentum=app_args.momentum, weight_decay=app_args.weight_decay)
-            train_fn(model=model_cpy, compression_scheduler=create_scheduler(model_cpy, zeros_mask_dict),
-                     optimizer=optimizer, epoch=-1)
-
-        # Test and record the performance of the pruned model
-        prec1, prec5, loss = test_func(model=model_cpy, loggers=None)
-        if (prec1, prec5) > (best_layer[0], best_layer[1]):
-            best_layer = (prec1, prec5, param_name, model_cpy, zeros_mask_dict)
-    return best_layer
+    for p in processes:
+        p.join()
+        (prec1, prec5, loss, zeros_mask_dict) = pqueue.get()
+        if (prec1, prec5, -loss) > (best_layer[0], best_layer[1], -best_layer[-1]):
+            best_layer = (prec1, prec5, param_name, model_cpy, zeros_mask_dict, loss)
+    return best_layer[:-1]
 
 
 def get_model_compute_budget(model, dataset, layers_to_prune=None):
@@ -118,14 +165,14 @@ def create_scheduler(model, zeros_mask_dict):
 def create_network_record_file():
     """Create the CSV file and write the column names"""
     fields = ['iteration', 'top1', 'param_name', 'normalized_macs', 'total_macs']
-    with open(os.path.join(msglogger.logdir, 'netadapt.csv'), 'w') as f:
+    with open(os.path.join(msglogger.logdir, 'greedy.csv'), 'w') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
 
 
 def record_network_details(fields):
     """Write the details of one network to a CSV file and create a checkpoint file"""
-    with open(os.path.join(msglogger.logdir, 'netadapt.csv'), 'a') as f:
+    with open(os.path.join(msglogger.logdir, 'greedy.csv'), 'a') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
 
@@ -158,9 +205,12 @@ def greedy_pruner(pruned_model, app_args, fraction_to_prune, pruning_step, test_
     iteration = 0
     while total_macs > fraction_to_prune * dense_total_macs:
         iteration += 1
-        prec1, prec5, param_name, pruned_model, zeros_mask_dict = find_most_robust_layer(pruned_model, pruning_step,
-                                                                                         test_fn, train_fn, app_args,
-                                                                                         resnet50_params)
+        training_epoch_duration = 1 - (total_macs / dense_total_macs)
+        prec1, prec5, param_name, pruned_model, zeros_mask_dict = find_most_robust_layer(iteration, pruned_model,
+                                                                                         pruning_step,
+                                                                                         test_fn, train_fn,
+                                                                                         app_args, resnet50_params,
+                                                                                         training_epoch_duration)
         assert distiller.sparsity_3D(zeros_mask_dict[param_name].mask) > 0
         # Physically remove filters
         distiller.remove_filters(pruned_model, zeros_mask_dict, arch, dataset, optimizer=None)
@@ -175,5 +225,5 @@ def greedy_pruner(pruned_model, app_args, fraction_to_prune, pruning_step, test_
     print(prec1, prec5, loss)
     scheduler = create_scheduler(pruned_model, zeros_mask_dict)
     save_checkpoint(0, arch, pruned_model, optimizer=None, best_top1=prec1, scheduler=scheduler,
-                    name='_'.join(("netadapt", str(fraction_to_prune))),
+                    name='_'.join(("greedy", str(fraction_to_prune))),
                     dir=msglogger.logdir)
