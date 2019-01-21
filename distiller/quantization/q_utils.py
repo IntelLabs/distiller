133868d71d4bd9dc592fe7461fd2df237e68bde8
Guy Jacob <guy.jacob@intel.com>
2019-01-21 16:48:06 +0200

    Updates to quantization parameters calculation:
    * Always include 0 in the range
    * Handle case where tensor is zeros only (fixes issue #115)
    * Add unit tests

--- distiller/quantization/q_utils.py
+++ distiller/quantization/q_utils.py
@@ -17,29 +17,70 @@
 import torch
 
 
+def _prep_saturation_val_tensor(sat_val):
+    is_scalar = not isinstance(sat_val, torch.Tensor)
+    out = torch.tensor(sat_val)
+    if not out.is_floating_point():
+        out = out.to(torch.float32)
+    if out.dim() == 0:
+        out = out.unsqueeze(0)
+    return is_scalar, out
+
+
 def symmetric_linear_quantization_params(num_bits, saturation_val):
+    is_scalar, sat_val = _prep_saturation_val_tensor(saturation_val)
+
+    if any(sat_val < 0):
+        raise ValueError('Saturation value must be >= 0')
+
     # Leave one bit for sign
     n = 2 ** (num_bits - 1) - 1
-    scale = n / saturation_val
-    if isinstance(scale, torch.Tensor):
-        zero_point = torch.zeros_like(scale)
-    else:
-        zero_point = 0.0
+
+    # If float values are all 0, we just want the quantized values to be 0 as well. So overriding the saturation
+    # value to 'n', so the scale becomes 1
+    sat_val[sat_val == 0] = n
+    scale = n / sat_val
+    zero_point = torch.zeros_like(scale)
+
+    if is_scalar:
+        # If input was scalar, return scalars
+        return scale.item(), zero_point.item()
     return scale, zero_point
 
 
 def asymmetric_linear_quantization_params(num_bits, saturation_min, saturation_max,
                                           integral_zero_point=True, signed=False):
+    scalar_min, sat_min = _prep_saturation_val_tensor(saturation_min)
+    scalar_max, sat_max = _prep_saturation_val_tensor(saturation_max)
+    is_scalar = scalar_min and scalar_max
+
+    if scalar_max and not scalar_min:
+        sat_max = sat_max.to(sat_min.device)
+    elif scalar_min and not scalar_max:
+        sat_min = sat_min.to(sat_max.device)
+
+    if any(sat_min > sat_max):
+        raise ValueError('saturation_min must be smaller than saturation_max')
+
     n = 2 ** num_bits - 1
-    scale = n / (saturation_max - saturation_min)
-    zero_point = scale * saturation_min
+
+    # Make sure 0 is in the range
+    sat_min = torch.min(sat_min, torch.zeros_like(sat_min))
+    sat_max = torch.max(sat_max, torch.zeros_like(sat_max))
+
+    diff = sat_max - sat_min
+    # If float values are all 0, we just want the quantized values to be 0 as well. So overriding the saturation
+    # value to 'n', so the scale becomes 1
+    diff[diff == 0] = n
+
+    scale = n / diff
+    zero_point = scale * sat_min
     if integral_zero_point:
-        if isinstance(zero_point, torch.Tensor):
-            zero_point = zero_point.round()
-        else:
-            zero_point = float(round(zero_point))
+        zero_point = zero_point.round()
     if signed:
         zero_point += 2 ** (num_bits - 1)
+    if is_scalar:
+        return scale.item(), zero_point.item()
     return scale, zero_point
 
 
