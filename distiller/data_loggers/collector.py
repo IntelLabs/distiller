Add standard deviation in quant stats collector + more review fixes

distiller/data_loggers/collector.py
@@ -19,11 +19,12 @@ import xlsxwriter
 import yaml
 import os
 from sys import float_info
-from collections import OrderedDict, namedtuple
+from collections import OrderedDict
 from contextlib import contextmanager
 import torch
 from torchnet.meter import AverageValueMeter
 import logging
+from math import sqrt
 import distiller
 msglogger = logging.getLogger()
 
@@ -92,7 +93,8 @@ class ActivationStatsCollector(object):
         Eligible modules are currently filtered by their class type.
         """
         is_leaf_node = len(list(module.children())) == 0
-        if is_leaf_node and (not self.classes or type(module) in self.classes):
+        register_all_class_types = not self.classes
+        if is_leaf_node and (register_all_class_types or (type(module) in self.classes)):
             self.fwd_hook_handles.append(module.register_forward_hook(self._activation_stats_cb))
             self._start_counter(module)
 
@@ -235,7 +237,9 @@ class RecordsActivationStatsCollector(ActivationStatsCollector):
         batch_min_list = to_np(torch.min(act, dim=1)).tolist()
         batch_max_list = to_np(torch.max(act, dim=1)).tolist()
         batch_mean_list = to_np(torch.mean(act, dim=1)).tolist()
-        if act.shape[1] == 1:
+        # If activation contains only a single element, standard-deviation is meaningless (and std() returns NaN)
+        # Return 0 instead
+        if act.shape[0] == act.numel():
             batch_std_list = to_np(torch.zeros(act.shape[0])).tolist()
         else:
             batch_std_list = to_np(torch.std(act, dim=1)).tolist()
@@ -297,7 +301,7 @@ class _QuantStatsRecord(object):
         records = OrderedDict()
         records['min'] = float_info.max
         records['max'] = -float_info.max
-        for stat_name in ['avg_min', 'avg_max', 'mean']:  # , 'M', 'std']:
+        for stat_name in ['avg_min', 'avg_max', 'mean', 'std']:
             records[stat_name] = 0
         records['shape'] = ''
         return records
@@ -315,7 +319,7 @@ class QuantCalibrationStatsCollector(ActivationStatsCollector):
       * Absolute min / max
       * Average min / max (calculate min / max per sample and average those)
       * Overall mean
-      * TODO: Overall standard-deviation
+      * Overall standard-deviation
     Calculated stats are saved to a YAML file.
 
     If a certain layer operates in-place, that layer's input stats will be overwritten by its output stats.
@@ -331,6 +335,13 @@ class QuantCalibrationStatsCollector(ActivationStatsCollector):
           in-place operations and disable them.
         inplace_attr_names (iterable): If disable_inplace_attrs is enabled, this is the list of attribute name
           that will be searched for.
+
+    TODO: Consider merging with RecordsActivationStatsCollector
+    Current differences between the classes:
+      * Track single value per-input/output-per-module for the entire run. Specifically, for standard deviation this
+        cannot be done by tracking per-activation std followed by some post-processing
+      * Track inputs in addition to outputs
+      * Different serialization (yaml vs xlsx)
     """
     def __init__(self, model, classes=None, inplace_runtime_check=False,
                  disable_inplace_attrs=False, inplace_attr_names=('inplace',)):
@@ -349,7 +360,16 @@ class QuantCalibrationStatsCollector(ActivationStatsCollector):
     def _activation_stats_cb(self, module, inputs, output):
         def update_mean(old_mean, new_val):
             return old_mean + (new_val - old_mean) / module.batch_idx
-            # return (old_mean * (self.batch_idx - 1) + new_val) / self.batch_idx
+
+        def update_std(values, old_std, old_mean, new_mean):
+            # See here:
+            # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_Online_algorithm
+            numel = values.numel() if isinstance(values, torch.Tensor) else values.size
+            total_values_so_far = numel * (module.batch_idx - 1)
+            M = (old_std ** 2) * (total_values_so_far - 1)
+            mean_diffs = (values - old_mean) * (values - new_mean)
+            M += mean_diffs.sum()
+            return sqrt((M / (total_values_so_far + numel - 1)).item())
 
         def update_record(record, tensor):
             act = tensor.view(tensor.size(0), -1)
@@ -360,15 +380,14 @@ class QuantCalibrationStatsCollector(ActivationStatsCollector):
             try:
                 record['avg_min'] = update_mean(record['avg_min'], min_per_sample.mean().item())
                 record['avg_max'] = update_mean(record['avg_max'], max_per_sample.mean().item())
-                record['mean'] = update_mean(record['mean'], act.mean().item())
+                new_mean = update_mean(record['mean'], act.mean().item())
+                record['std'] = update_std(tensor, record['std'], record['mean'], new_mean)
             except RuntimeError:
                 record['avg_min'] = update_mean(record['avg_min'], min_per_sample.cpu().numpy().mean().item(0))
                 record['avg_max'] = update_mean(record['avg_max'], max_per_sample.cpu().numpy().mean().item(0))
-                record['mean'] = update_mean(record['mean'], act.cpu().numpy().mean().item(0))
-
-            # TODO: Add std calculation
-            # See here:
-            # https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Welford's_Online_algorithm
+                new_mean = update_mean(record['mean'], act.cpu().numpy().mean().item(0))
+                record['std'] = update_std(tensor.cpu().numpy(), record['std'], record['mean'], new_mean)
+            record['mean'] = new_mean
 
             record['shape'] = distiller.size2str(tensor)
 

examples/classifier_compression/compress_classifier.py
@@ -115,11 +115,13 @@ def main():
             exit(1)
         # Use a well-known seed, for repeatability of experiments
         distiller.set_deterministic()
+        args.shuffle_test = False
     else:
         # This issue: https://github.com/pytorch/pytorch/issues/3659
         # Implies that cudnn.benchmark should respect cudnn.deterministic, but empirically we see that
         # results are not re-produced when benchmark is set. So enabling only if deterministic mode disabled.
         cudnn.benchmark = True
+        args.shuffle_test = True
 
     if args.cpu or not torch.cuda.is_available():
         # Set GPU index to -1 if using CPU
@@ -193,8 +195,10 @@ def main():
         msglogger.info('Quantization calibration stats collection enabled: Setting constant seeds and converting '
                        'model to serialized execution')
         distiller.set_deterministic()
+        args.shuffle_test = True
         model = distiller.make_non_parallel_copy(model)
         activations_collectors.update(create_quantization_stats_collector(model))
+        args.evaluate = True
 
     # Load the datasets: the dataset to load is inferred from the model name passed
     # in args.arch.  The default dataset is ImageNet, but if args.arch contains the
@@ -202,7 +206,7 @@ def main():
     train_loader, val_loader, test_loader, _ = apputils.load_data(
         args.dataset, os.path.expanduser(args.data), args.batch_size,
         args.workers, args.validation_size, args.deterministic,
-        shuffle_test=args.qe_calibration is not None)
+        shuffle_test=args.shuffle_test)
     msglogger.info('Dataset sizes:\n\ttraining=%d\n\tvalidation=%d\n\ttest=%d',
                    len(train_loader.sampler), len(val_loader.sampler), len(test_loader.sampler))
 
@@ -210,8 +214,9 @@ def main():
         sensitivities = np.arange(args.sensitivity_range[0], args.sensitivity_range[1], args.sensitivity_range[2])
         return sensitivity_analysis(model, criterion, test_loader, pylogger, args, sensitivities)
 
-    if args.evaluate or args.qe_calibration:
-        return evaluate_model(model, criterion, test_loader, pylogger, activations_collectors, args, compression_scheduler)
+    if args.evaluate:
+        return evaluate_model(model, criterion, test_loader, pylogger, activations_collectors, args,
+                              compression_scheduler)
 
     if args.compress:
         # The main use-case for this sample application is CNN compression. Compression
@@ -592,14 +597,7 @@ def evaluate_model(model, criterion, test_loader, loggers, activations_collector
 
     if args.quantize_eval:
         model.cpu()
-        if args.qe_config_file:
-            quantizer = distiller.config_component_from_file_by_class(model, args.qe_config_file,
-                                                                      'PostTrainLinearQuantizer')
-        else:
-            quantizer = quantization.PostTrainLinearQuantizer(model, args.qe_bits_acts, args.qe_bits_wts,
-                                                              args.qe_bits_accum, None, args.qe_mode, args.qe_clip_acts,
-                                                              args.qe_no_clip_layers, args.qe_per_channel,
-                                                              args.qe_stats_file)
+        quantizer = quantization.PostTrainLinearQuantizer.from_args(model, args)
         quantizer.prepare_model()
         model.to(args.device)
 