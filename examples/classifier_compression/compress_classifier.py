29cce220402dc306e3b367a85d568726b2c677b8
barrh <elhararb@gmail.com>
2019-05-13 10:44:27 +0300

    Fix quantization calibration logs
    Return file name from ActivationStatsCollector.save() and print it.

--- examples/classifier_compression/compress_classifier.py
+++ examples/classifier_compression/compress_classifier.py
@@ -755,12 +755,11 @@ def create_quantization_stats_collector(model):
 
 
 def save_collectors_data(collectors, directory):
-    """Utility function that saves all activation statistics to Excel workbooks
+    """Utility function that saves all activation statistics to Excel workbooks or YAMLs
     """
     for name, collector in collectors.items():
-        workbook = os.path.join(directory, name)
-        msglogger.info("Generating {}".format(workbook))
-        collector.save(workbook)
+        workbook_path = collector.save(os.path.join(directory, name))
+        msglogger.info("Generating {}".format(workbook_path))
 
 
 def check_pytorch_version():
