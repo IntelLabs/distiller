8b5c5d39cb4779d2c83f48ae96f50cc0006ccc97
levzlotnik <lev.zlotnik@intel.com>
2019-05-27 20:15:26 +0300

    Updated layer_fusions.py

--- distiller/quantization/layer_fusions.py
+++ distiller/quantization/layer_fusions.py
@@ -10,20 +10,37 @@ class FusedQuantBatchNorm(nn.Module):
         linear_module: the wrapped linear layer
         bn : batch normalization
     """
-    def __init__(self, linear_module, bn, freeze_bn_delay):
+    def __init__(self, linear_module, bn, freeze_bn_delay, frozen=False):
         super(FusedQuantBatchNorm, self).__init__()
         if not isinstance(linear_module, (nn.Linear, nn.Conv2d)) \
                 and not isinstance(bn, (nn.BatchNorm1d, nn.BatchNorm2d)):
             raise ValueError("Only supporting fusing nn.BatchNorm1d/nn.BatchNorm2d into nn.Linear/nn.Conv2d.")
+        if not bn.track_running_stats or not bn.affine:
+            raise ValueError("Quantization is only supported for BatchNorm which tracks runnins stats with"
+                             "affine weights.")
 
         self.linear_module = linear_module
         self.bn = bn
-        self.affine = bn.affine
-        self.track_running_stats = bn.track_running_stats
         self.freeze_bn_delay = freeze_bn_delay
-        self.freeze_bn = False  # Freezes bn stats
+        self.frozen = frozen  # Indicate whether the BatchNorm stats are frozen
 
     def forward(self, x):
         pass
 
+
+    def freeze(self):
+        """
+        Start using the long term BatchNorm stats.
+        We fuse these stats into the linear layer and only use it.
+        Now, the corrected weights for the linear layer are:
+            sigma = sqrt(bn.variance + bn.eps)
+            weights_corrected = bn.weights * linear.weights / sigma
+            bias_corrected = bn.weights * (linear.bias - bn.mean) / sigma + bn.bias =
+                           = linear.bias * bn.weights / sigma + (bn.bias - bn.weights * bn.mean / sigma)
+        """
+        inverse_sigma = torch.rsqrt(self.bn.running_var + self.bn.eps)
+        self.linear_module.weight.mul_(self.bn.weight * inverse_sigma)
+        self.linear_module.bias.mul_(self.bn.weight * inverse_sigma)
+        self.linear_module.bias.add_(self.bn.bias - self.bn.weight * self.bn.mean * inverse_sigma)
+        self.frozen = True
 
