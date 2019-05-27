e183da449c6ecc4c61896f9b5bf60977edc0ae4e
levzlotnik <lev.zlotnik@intel.com>
2019-05-27 19:15:37 +0300

    Updated layer_fusions.py

--- distiller/quantization/layer_fusions.py
+++ distiller/quantization/layer_fusions.py
@@ -1,9 +1,16 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class FusedQuantBatchNorm(nn.Module):
-    def __init__(self, linear_module, bn):
+    """
+    Wrapper for simulated fusing of BatchNorm into linear layers.
+    Args:
+        linear_module: the wrapped linear layer
+        bn : batch normalization
+    """
+    def __init__(self, linear_module, bn, freeze_bn_delay):
         super(FusedQuantBatchNorm, self).__init__()
         if not isinstance(linear_module, (nn.Linear, nn.Conv2d)) \
                 and not isinstance(bn, (nn.BatchNorm1d, nn.BatchNorm2d)):
@@ -11,9 +18,12 @@ class FusedQuantBatchNorm(nn.Module):
 
         self.linear_module = linear_module
         self.bn = bn
-
+        self.affine = bn.affine
+        self.track_running_stats = bn.track_running_stats
+        self.freeze_bn_delay = freeze_bn_delay
+        self.freeze_bn = False  # Freezes bn stats
 
     def forward(self, x):
-        if self.training:
+        pass
 
 
