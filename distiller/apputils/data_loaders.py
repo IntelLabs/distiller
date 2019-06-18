2cab774155e6b8e34e71da1adbc85ee6c997f442
Guy Jacob <guy.jacob@intel.com>
2019-06-18 14:23:51 +0300

    Input shape attribute in models (#292)
    Setting this attribute will become a requirement for some upcoming features
    
    * Add utility function to set input_shape attribute in models
    * Set this attribute in our classification models factory
      (models.create_model())
    * Add shape parameter in get_dummy_input() (in addition to dataset)

--- distiller/apputils/data_loaders.py
+++ distiller/apputils/data_loaders.py
@@ -47,22 +47,15 @@ def classification_num_classes(dataset):
             'imagenet': 1000}.get(dataset, None)
 
 
-def classification_get_dummy_input(dataset, device=None):
-    """Generate a representative dummy (random) input for the specified dataset.
-
-    If a device is specified, then the dummay_input is moved to that device.
-    """
+def classification_get_input_shape(dataset):
     if dataset == 'imagenet':
-        dummy_input = torch.randn(1, 3, 224, 224)
+        return 1, 3, 224, 224
     elif dataset == 'cifar10':
-        dummy_input = torch.randn(1, 3, 32, 32)
+        return 1, 3, 32, 32
     elif dataset == 'mnist':
-        dummy_input = torch.randn(1, 1, 28, 28)
+        return 1, 1, 28, 28
     else:
         raise ValueError("dataset %s is not supported" % dataset)
-    if device:
-        dummy_input = dummy_input.to(device)
-    return dummy_input
 
 
 def __dataset_factory(dataset):
