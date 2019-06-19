84c77a926e8af0f3964e80a74183611a0da4dda2
Neta Zmora <neta.zmora@intel.com>
2019-06-19 22:04:39 +0300

    spinningup fixes

--- examples/automated_deep_compression/rl_libs/spinningup/ddpg/core.py
+++ examples/automated_deep_compression/rl_libs/spinningup/ddpg/core.py
@@ -14,9 +14,19 @@ def mlp(x, hidden_sizes=(32,), activation=tf.tanh, output_activation=None):
     return tf.layers.dense(x, units=hidden_sizes[-1], activation=output_activation)
 
 # Neta
-def amc_mlp(x, a, hidden_sizes=(32,), activation=tf.tanh, output_activation=None):
-    x = tf.layers.dense(x, units=h[hidden_sizes[0]], activation=activation)
-    x = tf.layers.dense(tf.concat([x, a], axis=-1), units=h[hidden_sizes[1]], activation=activation)
+def amc_actor(x):
+    x = tf.layers.dense(x, units=300, activation=tf.nn.relu)
+    x = tf.layers.dense(x, units=300, activation=tf.nn.relu)
+    return tf.layers.dense(x, units=300, activation=tf.nn.sigmoid)
+
+# Neta
+def amc_critic(x, a, hidden_sizes=(32,), activation=tf.tanh, output_activation=None):
+    x = tf.layers.dense(x, units=300, activation=tf.nn.relu)
+    a = tf.layers.dense(a, units=300, activation=tf.nn.relu)
+    y = tf.concat([x, a], axis=-1)
+    y = tf.nn.relu(y)
+    #x = tf.layers.dense(tf.concat([x, a], axis=-1), units=h[hidden_sizes[1]], activation=activation)
+    y = tf.layers.dense(y, units=300, activation=tf.nn.relu)
     return tf.layers.dense(x, units=hidden_sizes[-1], activation=output_activation)
 
 
@@ -35,10 +45,11 @@ def mlp_actor_critic(x, a, hidden_sizes=(400,300), activation=tf.nn.relu,
     act_dim = a.shape.as_list()[-1]
     act_limit = action_space.high[0]
     with tf.variable_scope('pi'):
-        pi = act_limit * mlp(x, list(hidden_sizes)+[act_dim], activation, output_activation)
+        #pi = act_limit * mlp(x, list(hidden_sizes)+[act_dim], activation, output_activation)
+        pi = amc_actor(x)
     with tf.variable_scope('q'):
         #q = tf.squeeze(mlp(tf.concat([x,a], axis=-1), list(hidden_sizes)+[1], activation, None), axis=1)
-        q = tf.squeeze(amc_mlp(x, a, list(hidden_sizes)+[1], activation, None), axis=1)
+        q = tf.squeeze(amc_critic(x, a, list(hidden_sizes)+[1], activation, None), axis=1)
     with tf.variable_scope('q', reuse=True):
         #q_pi = tf.squeeze(mlp(tf.concat([x,pi], axis=-1), list(hidden_sizes)+[1], activation, None), axis=1)
         q_pi = tf.squeeze(mlp(x, pi, list(hidden_sizes)+[1], activation, None), axis=1)
