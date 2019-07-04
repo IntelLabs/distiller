2d7d8719f81abe56dd11a88a0914008591421271
Neta Zmora <neta.zmora@intel.com>
2019-07-04 16:40:48 +0300

    Non-functional changes

--- examples/automated_deep_compression/utils/data_dependencies.py
+++ examples/automated_deep_compression/utils/data_dependencies.py
@@ -18,7 +18,7 @@ import logging
 import torch
 import distiller
 
-
+__all__ = ["find_dependencies"]
 msglogger = logging.getLogger()
 
 
@@ -31,13 +31,13 @@ def find_dependencies(dependency_type, sgraph, layers, layer_name, dependencies_
     (dependency_type) in `layer_name`.
     """
     if dependency_type == "channels":
-        return find_dependencies_channels(sgraph, layers, layer_name, dependencies_list)
+        return _find_dependencies_channels(sgraph, layers, layer_name, dependencies_list)
     if dependency_type == "filters":
-        return find_dependencies_filters(sgraph, layers, layer_name, dependencies_list)
+        return _find_dependencies_filters(sgraph, layers, layer_name, dependencies_list)
     raise ValueError("%s is not a valid dependency type" % dependency_type)
 
 
-def find_dependencies_channels(sgraph, layers, layer_name, dependencies_list):
+def _find_dependencies_channels(sgraph, layers, layer_name, dependencies_list):
     # Find all instances of Convolution layers that immediately preceed this layer
     predecessors = sgraph.predecessors_f(layer_name, ['Conv'])
     for predecessor in predecessors:
@@ -46,7 +46,7 @@ def find_dependencies_channels(sgraph, layers, layer_name, dependencies_list):
 
         if prev['attrs']['group'] == prev['attrs']['n_ifm']:
             # This is a group-wise convolution, and a special one at that (groups == in_channels).
-            find_dependencies_channels(sgraph, layers, predecessor, dependencies_list)
+            _find_dependencies_channels(sgraph, layers, predecessor, dependencies_list)
         elif prev['attrs']['group'] != 1:
             raise ValueError("Distiller AutoCompression currently does not "
                              "handle this convolution groups configuration {} "
@@ -55,7 +55,7 @@ def find_dependencies_channels(sgraph, layers, layer_name, dependencies_list):
 
 
 # todo: remove the 'layers' parameter
-def find_dependencies_filters(sgraph, layers, layer_name, dependencies_list):
+def _find_dependencies_filters(sgraph, layers, layer_name, dependencies_list):
     # Find all instances of Convolution or FC (GEMM) layers that immediately follow this layer
     successors = sgraph.successors_f(layer_name, ['Conv', 'Gemm'])    
     for successor in successors:
@@ -64,7 +64,7 @@ def find_dependencies_filters(sgraph, layers, layer_name, dependencies_list):
         if next['type'] == 'Conv':
             if next['attrs']['group'] == next['attrs']['n_ifm']:
                 # This is a group-wise convolution, and a special one at that (groups == in_channels).
-                find_dependencies_filters(sgraph, layers, successor, dependencies_list)
+                _find_dependencies_filters(sgraph, layers, successor, dependencies_list)
             elif next['attrs']['group'] != 1:
                 raise ValueError("Distiller AutoCompression currently does not handle this conv.groups configuration")    
  
