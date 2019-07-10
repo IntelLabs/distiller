43548debc7e8aea743889278d0f1076eeaadc67e
Guy Jacob <guy.jacob@intel.com>
2019-07-10 14:40:58 +0300

    Post-Train Quantization: BN folding and "net-aware quantization" (#313)
    * "Net-aware quantization" - using the term coined in
      https://arxiv.org/abs/1811.09886. (section 3.2.2).
      Refers to considering sequences of modules when quantizing. This 
      isn't exactly layer fusion - we modify activation stats prior to
      setting quantization parameters, to make sure that when a module
      is followed by certain activation functions, only the relevant
      ranges are quantized. We do this for:
        * ReLU - Clip all negative values
        * Tanh / Sigmoid - Clip according to the (approximated) saturation
          values for these functions. We use [-4, 4] for tanh and [-6, 6]
          for sigmoid.
    
    * Perform batch-norm folding before post-training quantization.
      Batch-norm parameters are folded into the parameters of the previous
      layer and the BN layer is replaced with an identity module.
    
    * Both BN folding and "net-aware" are now automatically executed
      in PostTrainLinearQuantizer (details of this change below)
    
    * BN folding enabled by new generic mechanism to "fuse" module
      sequences (at the Python API level)
        * First module in sequence is replaced/modified by a user-provided
          function, rest of moudles replaced with nn.Identity
    
    * Quantizer changes:
      * Optionally create adjacency map during prepare_model
      * Subclasses may enforce adjacency map creation
      * Refatcoring: Replace _prepare_model_impl with pre and post
        override-able "callbacks", so core functionality is always executed
    
    * PostTrainLinearQuantizer Changes:
      * Enforce creation of adjacency map. This means users must now pass a
        dummy input to PostTrainLinearQuantizer.prepare_model
      * Before module replacement - Apply BN folding and stats updates according
        to net-aware quantization
    
    * Updated the language model quantization tutorial to reflect the new
      functionality
    
    * Updated the image classification post-train quantization samples
      (command line and YAML)
    
    * Other changes:
      * Distller LSTM implementation:
        Replace the ModuleList for cells with a plain list. The PyTorch trace
        mechanism doesn't "see" ModuleList objects, it only sees the 
        contained modules. This means that the "scopeName" of these modules
        isn't complete, which makes it impossible to match op names in 
        SummaryGraph to modules in the Python model.
      * ActivationStatsCollector: Ignore nn.Identity modules

--- distiller/quantization/quantizer.py
+++ distiller/quantization/quantizer.py
@@ -112,6 +112,8 @@ class Quantizer(object):
         if train_with_fp_copy and optimizer is None:
             raise ValueError('optimizer cannot be None when train_with_fp_copy is True')
 
+        self.adjacency_map = None  # To be populated during prepare_model()
+
         self.default_qbits = QBits(acts=bits_activations, wts=bits_weights, bias=bits_bias)
         self.overrides = overrides
 
@@ -188,7 +190,7 @@ class Quantizer(object):
     def _add_override_entry(self, module_name, entry):
         self.module_overrides_map[module_name] = entry
 
-    def prepare_model(self):
+    def prepare_model(self, dummy_input=None):
         """
         Traverses the model and replaces sub-modules with quantized counterparts according to the bit-width
         and overrides configuration provided to __init__(), and according to the replacement_factory as
@@ -209,18 +211,15 @@ class Quantizer(object):
             with a reference to 'new_relu1'. Any override configuration made specifically for 'self.relu2'
             will be ignored. A warning message will be shown.
         """
-        self._prepare_model_impl()
-
-        msglogger.info('Quantized model:\n\n{0}\n'.format(self.model))
-
-    def _prepare_model_impl(self):
-        r"""
-        Iterates over the model and replaces modules with their quantized counterparts as defined by
-        self.replacement_factory
-        """
         msglogger.info('Preparing model for quantization using {0}'.format(self.__class__.__name__))
-        self._pre_process_container(self.model)
 
+        if dummy_input is not None:
+            summary_graph = distiller.SummaryGraph(self.model, dummy_input)
+            self.adjacency_map = summary_graph.adjacency_map(dedicated_modules_only=False)
+
+        self._pre_prepare_model(dummy_input)
+
+        self._pre_process_container(self.model)
         for module_name, module in self.model.named_modules():
             qbits = self.module_qbits_map[module_name]
             curr_parameters = dict(module.named_parameters())
@@ -244,7 +243,22 @@ class Quantizer(object):
             new_optimizer = optimizer_type(self._get_updated_optimizer_params_groups(), **self.optimizer.defaults)
             self.optimizer.__setstate__({'param_groups': new_optimizer.param_groups})
 
+        self._post_prepare_model()
+
+        msglogger.info('Quantized model:\n\n{0}\n'.format(self.model))
+
+    def _pre_prepare_model(self, dummy_input):
+        pass
+
     def _pre_process_container(self, container, prefix=''):
+        def replace_msg(module_name, modules=None):
+            msglogger.debug('Module ' + module_name)
+            if modules:
+                msglogger.debug('\tReplacing: {}.{}'.format(modules[0].__module__, modules[0].__class__.__name__))
+                msglogger.debug('\tWith:      {}.{}'.format(modules[1].__module__, modules[1].__class__.__name__))
+            else:
+                msglogger.debug('\tSkipping')
+
         # Iterate through model, insert quantization functions as appropriate
         for name, module in container.named_children():
             full_name = prefix + name
@@ -254,18 +268,17 @@ class Quantizer(object):
                               ' Replacing with reference the same wrapper.'.format(full_name, previous_name),
                               UserWarning)
                 if previous_wrapper:
-                    msglogger.debug('Module {0}: Replacing \n{1} with \n{2}'.
-                                    format(full_name, module, previous_wrapper))
+                    replace_msg(full_name, (module, previous_wrapper))
                     setattr(container, name, previous_wrapper)
                 else:
-                    msglogger.debug('Module {0}: Skipping \n{1}.'.format(full_name, module))
+                    replace_msg(full_name)
                 continue
             current_qbits = self.module_qbits_map[full_name]
             if current_qbits.acts is None and current_qbits.wts is None:
                 if self.module_overrides_map[full_name]:
                     raise ValueError("Adding overrides while not quantizing is not allowed.")
                 # We indicate this module wasn't replaced by a wrapper
-                msglogger.debug('Module {0}: Skipping \n{1}.'.format(full_name, module))
+                replace_msg(full_name)
                 self.modules_processed[module] = full_name, None
                 continue
 
@@ -279,7 +292,7 @@ class Quantizer(object):
                                         as override arguments for %s. Allowed kwargs: %s"""
                                     % (type(self), list(invalid_kwargs), type(module), list(valid_kwargs)))
                 new_module = replace_fn(module, full_name, self.module_qbits_map, **valid_kwargs)
-                msglogger.debug('Module {0}: Replacing \n{1} with \n{2}'.format(full_name, module, new_module))
+                replace_msg(full_name, (module, new_module))
                 # Add to history of prepared submodules
                 self.modules_processed[module] = full_name, new_module
                 setattr(container, name, new_module)
@@ -307,6 +320,9 @@ class Quantizer(object):
         """
         # Default implementation - just return all model parameters as one group
         return [{'params': self.model.parameters()}]
+
+    def _post_prepare_model(self):
+        pass
 
     def quantize_params(self):
         """
