cbc78e579075115427f1555fb4cc884aa2f81277
Neta Zmora <neta.zmora@intel.com>
2019-07-24 01:42:18 +0300

    Renamved random_agent and cleaned-up the implementation

--- examples/automated_deep_compression/rl_libs/random/random_agent.py
+++ examples/automated_deep_compression/rl_libs/random/random_agent.py
@@ -23,46 +23,24 @@ msglogger = logging.getLogger()
 
 
 class RandomADCActionSpace(object):
-    def __init__(self, low, high, std, mean, num_layers):
+    def __init__(self, low, high):
         self.action_low = low
         self.action_high = high
-        self.layer = 0
-        self.num_layers = num_layers
-        self.means = [mean] * num_layers
-        #self.means = [high-low] * self.num_layers
-        #self.means = [0.9, 0.9, 0.9, 0.9, 0.9, 0.8, 0.8, 0.7, 0.7, 0.6, 0.6, 0.5, 0.5]
-        self.std = std
 
     def sample(self):
         return random.uniform(self.action_low, self.action_high)
-        # action_values_mean = self.means[self.layer]
-        # action_values_std = self.std
-        # normalized_low = (self.action_low - action_values_mean) / action_values_std
-        # normalized_high = (self.action_high - action_values_mean) / action_values_std
-        # distribution = truncnorm(normalized_low, normalized_high, loc=action_values_mean, scale=action_values_std)
-        # action = distribution.rvs(1)
-        # # action = np.random.normal(self.means[self.layer], self.std)
-        # # action = min(self.action_high, max(action, self.action_low))
-        # self.layer = (self.layer + 1) % self.num_layers
-        # return action
-
-    def set_cfg(self, means, std):
-        self.means = [0.01*m for m in self.means] + [0.99*m for m in means]
-        self.std = std
 
 
 def random_agent(env):
     """Random ADC agent"""
     action_range = env.amc_cfg.action_range
-    best_episode = [-1000, None]
-    update_rate = 5
-    num_layers = env.net_wrapper.num_layers()
-    env.action_space = RandomADCActionSpace(action_range[0], action_range[1],
-                                            std=0.35, mean=env.amc_cfg.target_density, num_layers=num_layers)
-    for ep in range(1000):
+    best_episode = [float("-inf"), None]
+    env.action_space = RandomADCActionSpace(action_range[0], action_range[1])
+    n_episodes = env.amc_cfg.ddpg_cfg.num_training_episodes + env.amc_cfg.ddpg_cfg.num_heatup_episodes 
+    for ep in range(n_episodes):
         observation = env.reset()
         action_config = []
-        for t in range(100):
+        for t in range(env.steps_per_episode):
             #env.render(0, 0)
             msglogger.info("[episode={}:{}] observation = {}".format(ep, t, observation))
             # take a random action
@@ -75,8 +53,4 @@ def random_agent(env):
                 msglogger.info("New solution found: episode={} reward={} config={}".format(ep, reward, action_config))
                 break
         if reward > best_episode[0]:
-            best_episode[0] = reward
-            best_episode[1] = action_config
-        if ep % update_rate == 0:
-            env.action_space.set_cfg(means=best_episode[1], std=0.4)
-            best_episode = [-1000, None]
+            best_episode = (reward, action_config)
