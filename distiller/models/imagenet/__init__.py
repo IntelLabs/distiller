46e4690c745a8d7d8e714d491a1e1cf83cddf75e
Neta Zmora <neta.zmora@intel.com>
2019-08-13 23:49:12 +0300

    Models: add mobilenet_v1 with droupout
    Adding mobilenet_v1 with dropout before the classifier.
    
    Source: https://github.com/mit-han-lab/amc-compressed-models/blob/master/models/mobilenet_v1.py

--- distiller/models/imagenet/__init__.py
+++ distiller/models/imagenet/__init__.py
@@ -17,6 +17,7 @@
 """This package contains ImageNet image classification models not found in torchvision"""
 
 from .mobilenet import *
+from .mobilenet_droupout import *
 from .preresnet_imagenet import *
 from .alexnet_batchnorm import *
 from .resnet_earlyexit import *
