b480b7605d696ca4deeda6376debb02c5689f718
levzlotnik <lev.zlotnik@intel.com>
2019-08-18 20:37:04 +0300

    Added greedy search

--- distiller/quantization/ptq_greedy_search.py
+++ distiller/quantization/ptq_greedy_search.py
@@ -16,5 +16,95 @@
 r"""
 Here we implement the greedy search algorithm for automatic quantization.
 """
-from .range_linear import PostTrainLinearQuantizer
+import torch
+import torch.nn as nn
+from .range_linear import PostTrainLinearQuantizer, ClipMode
+from distiller.summary_graph import SummaryGraph
+import distiller.apputils as apputils
+from collections import OrderedDict
+import logging
+
+msglogger = logging.getLogger()
+
+
+class _OpRank:
+    def __init__(self, adj_entry, rank=None):
+        self.adj_entry = adj_entry
+        self._rank = rank or 0
+
+    @property
+    def rank(self):
+        return self._rank
+
+    @rank.setter
+    def rank(self, val):
+        self._rank = max(val, self._rank)
+
+    def __repr__(self):
+        return '_OpRank(\'%s\' | %d)' % (self.adj_entry.op_meta.name, self.rank)
+
+
+def layers_quant_order(model, dummy_input, recurrent=False):
+    """
+    Prepares an ordered list of layers to quantize sequentially. This list has all the layers ordered by their
+    topological order in the graph.
+    Args:
+        model (nn.Module): the model to quantize.
+        dummy_input (torch.Tensor): an input to be passed through the model.
+        recurrent (bool): indication on whether the model might have recurrent connections.
+    """
+    adj_map = SummaryGraph(model, dummy_input).adjacency_map()
+    ranked_ops = {k: _OpRank(v, 0) for k, v in adj_map.items()}
+
+    def _recurrent_ancestor(ranked_ops_dict, dest_op_name, src_op_name):
+        def _is_descendant(parent_op_name, dest_op_name):
+            successors_names = [op.name for op in adj_map[parent_op_name].successors]
+            if dest_op_name in successors_names:
+                return True
+            for succ_name in successors_names:
+                if _is_descendant(succ_name, dest_op_name):
+                    return True
+            return False
+
+        return _is_descendant(dest_op_name, src_op_name) and \
+            (0 < ranked_ops_dict[dest_op_name].rank < ranked_ops_dict[src_op_name].rank)
+
+    def rank_op(ranked_ops_dict, op_name, rank):
+        ranked_ops_dict[op_name].rank = rank
+        for child_op in adj_map[op_name].successors:
+            # In recurrent models: if a successor is also an ancestor - we don't increment its rank.
+            if not recurrent or not _recurrent_ancestor(ranked_ops_dict, child_op.name, op_name):
+                rank_op(ranked_ops_dict, child_op.name, ranked_ops_dict[op_name].rank + 1)
+
+    roots = [k for k, v in adj_map.items() if len(v.predecessors) == 0]
+    for root_op_name in roots:
+        rank_op(ranked_ops, root_op_name, 0)
+
+    # Take only the modules from the original model
+    module_list = dict(model.named_modules())
+    ret = sorted([k for k in ranked_ops.keys() if k in module_list],
+                 key=lambda k: ranked_ops[k].rank)
+    # Check that only the actual roots have a rank of 0
+    assert {k for k in ret if ranked_ops[k].rank == 0} <= set(roots)
+
+    return ret
+
+
+def greedy_search(model, dummy_input, eval_fn):
+    """
+    Perform greedy search on Post Train Quantization configuration for the model.
+    Args:
+        model (nn.Module): the model to quantize
+        dummy_input (torch.Tensor): a dummy input to be passed to the model
+        eval_fn (function): Test/Evaluation function for the model. It must have an argument named 'model' that
+          accepts the model. All other arguments should be set in advance (can be done using functools.partial), or
+          they will be left with their default values.
+    Returns:
+        (quantized_model, best_overrides_dict)
+    """
+    clip_modes_to_search = [ClipMode.NONE, ClipMode.AVG, ClipMode.GAUSS, ClipMode.LAPLACE]
+    best_overrides_dict = OrderedDict()
+    overrides_dict = OrderedDict()
+
+
 
