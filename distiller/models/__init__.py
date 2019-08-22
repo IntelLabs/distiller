2f76027ef8d9ce81b6e99e47c1da32c8912dd549
Neta Zmora <neta.zmora@intel.com>
2019-08-22 18:57:14 +0300

    apputils/checkpoint.py: load_checkpoint can be called w/o specifying the model
    This is inspired by @barrh’s PR https://github.com/NervanaSystems/distiller/pull/246
    but it at a “slower-integration-pace” and w/o changing APIs.
    
    1. create_model() attaches model attributes (arch, dataset, is_parallel) to created models.
    2. save_checkpoint() stores the new model attributes with checkpoint metadata
    3. load_checkpoint() can be invoked with model=None, in which case we attempt
    to create the model from the stored checkpoint metadata.

--- distiller/models/__init__.py
+++ distiller/models/__init__.py
@@ -111,14 +111,19 @@ def create_model(pretrained, dataset, arch, parallel=True, device_ids=None):
                                                                      arch, dataset))
     if torch.cuda.is_available() and device_ids != -1:
         device = 'cuda'
-        if (arch.startswith('alexnet') or arch.startswith('vgg')) and parallel:
-            model.features = torch.nn.DataParallel(model.features, device_ids=device_ids)
-        elif parallel:
-            model = torch.nn.DataParallel(model, device_ids=device_ids)
+        if parallel:
+            if arch.startswith('alexnet') or arch.startswith('vgg'):
+                model.features = torch.nn.DataParallel(model.features, device_ids=device_ids)
+            else:
+                model = torch.nn.DataParallel(model, device_ids=device_ids)
     else:
         device = 'cpu'
 
+    # Cache some attributes which describe the model
     _set_model_input_shape_attr(model, arch, dataset, pretrained, cadene)
+    model.arch = arch
+    model.dataset = dataset
+    model.is_parallel = parallel
     return model.to(device)
 
 
