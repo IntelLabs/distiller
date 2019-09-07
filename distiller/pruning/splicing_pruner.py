e01e998f877a682054afe4ed222617dcd778ad92
Neta Zmora <neta.zmora@intel.com>
2019-09-07 10:30:42 +0300

    some more refactoring - still nothing serious

--- distiller/pruning/splicing_pruner.py
+++ distiller/pruning/splicing_pruner.py
@@ -52,39 +52,46 @@ class SplicingPruner(_ParameterPruner):
         else:
             sensitivity = self.sensitivities[param_name]
 
-        if not hasattr(param, '_std'):
-            # Compute the mean and standard-deviation once, and cache them.
-            param._std = torch.std(param.abs()).item()
-            param._mean = torch.mean(param.abs()).item()
-
         if self.sensitivity_multiplier > 0:
             # Linearly growing sensitivity - for now this is hard-coded
             starting_epoch = meta['starting_epoch']
             current_epoch = meta['current_epoch']
             sensitivity *= (current_epoch - starting_epoch) * self.sensitivity_multiplier + 1
 
-        threshold_low = (param._mean + param._std * sensitivity) * self.low_thresh_mult
-        threshold_hi = (param._mean + param._std * sensitivity) * self.hi_thresh_mult
-
         if zeros_mask_dict[param_name].mask is None:
             zeros_mask_dict[param_name].mask = torch.ones_like(param)
+        zeros_mask_dict[param_name].mask = self.create_mask(param,
+                                                            zeros_mask_dict[param_name].mask,
+                                                            sensitivity,
+                                                            self.low_thresh_mult,
+                                                            self.hi_thresh_mult)
 
-        # This code performs the code in equation (3) of the "Dynamic Network Surgery" paper:
-        #
-        #           0    if a  > |W|
-        # h(W) =    mask if a <= |W| < b
-        #           1    if b <= |W|
-        #
-        # h(W) is the so-called "network surgery function".
-        # mask is the mask used in the previous iteration.
-        # a and b are the low and high thresholds, respectively.
-        # We followed the example implementation from Yiwen Guo in Caffe, and used the
-        # weight tensor's starting mean and std.
-        # This is very similar to the initialization performed by distiller.SensitivityPruner.
-    
-        mask = zeros_mask_dict[param_name].mask
-        zeros, ones = torch.tensor([0]).type(mask.type()), torch.tensor([1]).type(mask.type())
-        weights_abs = param.abs()
-        new_mask = torch.where(threshold_low > weights_abs, zeros, mask)
-        new_mask = torch.where(threshold_hi <= weights_abs, ones, new_mask)
-        zeros_mask_dict[param_name].mask = new_mask
+    @staticmethod
+    def create_mask(param, current_mask, sensitivity, low_thresh_mult, hi_thresh_mult):
+        with torch.no_grad():
+            if not hasattr(param, '_std'):
+                # Compute the mean and standard-deviation once, and cache them.
+                param._std = torch.std(param.abs()).item()
+                param._mean = torch.mean(param.abs()).item()
+
+            threshold_low = (param._mean + param._std * sensitivity) * low_thresh_mult
+            threshold_hi = (param._mean + param._std * sensitivity) * hi_thresh_mult
+
+            # This code performs the code in equation (3) of the "Dynamic Network Surgery" paper:
+            #
+            #           0    if a  > |W|
+            # h(W) =    mask if a <= |W| < b
+            #           1    if b <= |W|
+            #
+            # h(W) is the so-called "network surgery function".
+            # mask is the mask used in the previous iteration.
+            # a and b are the low and high thresholds, respectively.
+            # We followed the example implementation from Yiwen Guo in Caffe, and used the
+            # weight tensor's starting mean and std.
+            # This is very similar to the initialization performed by distiller.SensitivityPruner.
+
+            zeros, ones = torch.zeros_like(current_mask), torch.ones_like(current_mask)
+            weights_abs = param.abs()
+            new_mask = torch.where(threshold_low > weights_abs, zeros, current_mask)
+            new_mask = torch.where(threshold_hi <= weights_abs, ones, new_mask)
+            return new_mask
