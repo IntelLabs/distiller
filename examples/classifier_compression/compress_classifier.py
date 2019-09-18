8d55ab150e75035ecc5973a32b33b3cd9875cd89
Neta Zmora <neta.zmora@intel.com>
2019-09-18 11:00:07 +0300

    Odds and ends commit
    A bundle of very small, and mostly non-functional, changes to the code.
    Mostly they are unrelated to each other
    ../../../distiller/apputils/checkpoint.py – add info to exception
    
    ../../../distiller/apputils/image_classifier.py – remove unused
    `--extras` command-line argument
    
    ../../../distiller/thinning.py – code refactoring (non-functional)
    except for adding a new public API: contract_model()
    
    ../../classifier_compression/compress_classifier.py – use
    contract_model() when using `--thinnify`
    
    ../../lottery_ticket/README.md – remove illegal characters in
    the text

--- examples/classifier_compression/compress_classifier.py
+++ examples/classifier_compression/compress_classifier.py
@@ -120,7 +120,7 @@ def handle_subapps(model, criterion, optimizer, compression_scheduler, pylogger,
         #zeros_mask_dict = distiller.create_model_masks_dict(model)
         assert args.resumed_checkpoint_path is not None, \
             "You must use --resume-from to provide a checkpoint file to thinnify"
-        distiller.remove_filters(model, compression_scheduler.zeros_mask_dict, args.arch, args.dataset, optimizer=None)
+        distiller.contract_model(model, compression_scheduler.zeros_mask_dict, args.arch, args.dataset, optimizer=None)
         apputils.save_checkpoint(0, args.arch, model, optimizer=None, scheduler=compression_scheduler,
                                  name="{}_thinned".format(args.resumed_checkpoint_path.replace(".pth.tar", "")),
                                  dir=msglogger.logdir)
