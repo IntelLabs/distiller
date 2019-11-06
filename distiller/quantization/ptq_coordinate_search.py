6e574282d256769a4ed003e0226314b8ec2caf90
levzlotnik <lev.zlotnik@intel.com>
2019-11-06 14:25:30 +0200

    started implementation

---
+++ distiller/quantization/ptq_coordinate_search.py
@@ -0,0 +1,45 @@
+#
+# Copyright (c) 2019 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import torch
+import torch.nn as nn
+from distiller.quantization.range_linear import PostTrainLinearQuantizer, ClipMode, LinearQuantMode
+from distiller.summary_graph import SummaryGraph
+from distiller.model_transforms import fold_batch_norms
+import distiller.modules
+from distiller.data_loggers import collect_quant_stats
+from distiller.models import create_model
+from collections import OrderedDict, defaultdict
+import logging
+from copy import deepcopy
+import distiller.apputils.image_classifier as classifier
+import os
+import distiller.apputils as apputils
+import re
+import argparse
+import scipy.optimize as opt
+import numpy as np
+
+
+def quant_params_dict2vec(p_dict):
+    keys, vals = zip(*p_dict.items())  # unzip the list of tuples
+    vals = np.array([val.item() for val in vals])
+    return keys, vals
+
+
+def quant_params_vec2dict(keys, vals):
+    return OrderedDict(zip(keys, vals))
+
+
