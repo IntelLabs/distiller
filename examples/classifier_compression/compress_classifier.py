0f751cf21af1c9291adf4a4949a6203073aabc6d
barrh <elhararb@gmail.com>
2019-11-06 13:58:11 +0200

    Deepcopy Namespace argument in Classifier class init function
    This fixes a bug where classifier_obj.args will being modified
    by the caller inadvertently.

--- examples/classifier_compression/compress_classifier.py
+++ examples/classifier_compression/compress_classifier.py
@@ -73,7 +73,7 @@ def main():
     app = ClassifierCompressorSampleApp(args, script_dir=os.path.dirname(__file__))
     if app.handle_subapps():
         return
-    init_knowledge_distillation(app.args,  app.model, app.compression_scheduler)
+    init_knowledge_distillation(app.args, app.model, app.compression_scheduler)
     app.run_training_loop()
     # Finally run results on the test set
     return app.test()
