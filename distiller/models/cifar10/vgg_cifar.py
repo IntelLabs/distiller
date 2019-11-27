f92656c7f4e3d32458e6509caeea9204e81d27de
Neta Zmora <neta.zmora@intel.com>
2019-11-28 00:42:15 +0200

    Revert "Cifar models: remove explicit parameters initialization"
    Said commit was wrong: the default inializations in pytorch are not
    the same as in our code.  For example, the default convolution
    weight initialization uses Kaiming-uniform, while we used
    Kaiming-normal.
    For backward comparability of the model behavior, we need to
    revert to the old behavior.
    This reverts commit 6913687f61425e9102151d8a45caaee63ec40618.

--- distiller/models/cifar10/vgg_cifar.py
+++ distiller/models/cifar10/vgg_cifar.py
@@ -32,16 +32,31 @@ __all__ = [
 
 
 class VGGCifar(nn.Module):
-    def __init__(self, features, num_classes=10):
+    def __init__(self, features, num_classes=10, init_weights=True):
         super(VGGCifar, self).__init__()
         self.features = features
         self.classifier = nn.Linear(512, num_classes)
+        if init_weights:
+            self._initialize_weights()
 
     def forward(self, x):
         x = self.features(x)
         x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
 
 def make_layers(cfg, batch_norm=False):
