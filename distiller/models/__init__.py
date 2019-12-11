8a2723063273d5ecc2e4d0fe2a1fc84c068ec242
Guy Jacob <guy.jacob@intel.com>
2019-12-11 10:55:32 +0200

    Add support for wide-resnet models that exist in torchvision

--- distiller/models/__init__.py
+++ distiller/models/__init__.py
@@ -39,7 +39,7 @@ SUPPORTED_DATASETS = ('imagenet', 'cifar10', 'mnist')
 # ResNet special treatment: we have our own version of ResNet, so we need to over-ride
 # TorchVision's version.
 RESNET_SYMS = ('ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
-               'resnext50_32x4d', 'resnext101_32x8d')
+               'resnext50_32x4d', 'resnext101_32x8d', 'wide_resnet50_2', 'wide_resnet101_2')
 
 TORCHVISION_MODEL_NAMES = sorted(
                             name for name in torch_models.__dict__
